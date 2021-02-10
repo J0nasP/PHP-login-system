95e62f89257ffae4402e48dc8a8f59d78362cbe0
J0nasP <petersen0108@gmail.com>
2021-02-10 13:13:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Project</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo" alt="Blogs logo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">About Us</a></li>
+                <li><a href="blog.php">Find Blogs</a></li>
+                <li><a href="signup.php">Sign up</a></li>
+                <li><a href="login.php">Log in</a></li>
+            </ul>
+        </div>
+    </nav>
+
+<div class="wrapper">
+
+    <section class="index-intro">
+        <h1> This is an Introduction</h1>
+        <p>Here is an important paragraph that explains the purpose of the the site</p>
+    </section>
+
+    <section class="index-categories">
+        <h2>Some basic categories</h2>
+        <div class="index-categories-list">
+        <div>
+            <h3>Fun stuff</h3>
+        </div>
+        <div>
+            <h3>Serious stuff</h3>
+        </div>
+    </section>
+
+</div>
+</body>
+</html>
