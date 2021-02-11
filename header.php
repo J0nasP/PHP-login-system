ce8641f4959e7e669710555b61779132c9f6df91
J0nasP <petersen0108@gmail.com>
2021-02-11 11:54:21 +0100

    Server database prepared

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Project</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo1.jpg" alt="Blogs logo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">About Us</a></li>
+                <li><a href="blog.php">Find Blogs</a></li>
+                <li><a href="signup.php">Sign up</a></li>
+                <li><a href="login.php">Log in</a></li>
+            </ul>
+        </div>
+    </nav>
+<div class="wrapper" >
--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>PHP Project</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    
-    <nav>
-        <div class="wrapper">
-            <a href="index.php"><img src="img/logo" alt="Blogs logo"></a>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="discover.php">About Us</a></li>
-                <li><a href="blog.php">Find Blogs</a></li>
-                <li><a href="signup.php">Sign up</a></li>
-                <li><a href="login.php">Log in</a></li>
-            </ul>
-        </div>
-    </nav>
-
-<div class="wrapper">
+<?php
+    include_once 'header.php';
+?>
 
     <section class="index-intro">
         <h1> This is an Introduction</h1>
@@ -45,9 +24,6 @@
         </div>
     </section>
 
-</div>
-</body>
-</html>
-
-
-<script src="js/script.js"></script>
+<?php
+    include_once 'footer.php';
+?>
